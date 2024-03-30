d03e9ef39ee1462f434d070f3e341ae0fe2f06e3
nb-prog01 <nirajbhadoria007@gmail.com>
2024-03-31 00:10:39 +0530

    Paytm middleware update

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors=require('cors');
 
-const mainRouter=require('./routes/index');
-const app=express();
-const port=3000;
-
 app.use(express.json());
 app.use(cors());
+
+const mainRouter=require('./routes/index');
+
 app.use('/api/v1',mainRouter)
 
+const app=express();
+const port=3000;
 
 app.listen(port)
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,93 @@
 const express=require('express');
 
+const { User } = require('../db');
+const { JWT_SECRET } = require('../config');
+const { jwt }=require('jsonwebtoken');
+const { zod }=require('zod');
 const router=express.Router();
+
+//singup and signin routes
+
+const signupSchema=zod.object({
+    usename:zod.string().email(),
+    password:zod.string(),
+    firstName:zod.string(),
+    lastName:zod.string(),
+})
+
+router.post("/signup",async (req,res)=>{
+    const body=req.body;
+    const{success}=signupSchema.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message:"Email already taken / Incorrect inputs"
+        })
+    }
+    
+    const existinguser=await User.findOne({
+        username:body.username
+    })
+
+    if(existinguser._id){
+        return res.status(411).json({
+            message:"Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser=await User.create(body);
+    //OR
+    // const dbUser=await User.create({
+    //     username: body.username,
+    //     password: body.password,
+    //     firstName: body.firstName,
+    //     lastName: body.lastName,
+    // });
+
+    const token=jwt.sign({
+        userId:dbUser._id
+    },JWT_SECRET)
+
+    res.json({
+        message: "User created successfully",
+        token:token
+    })
+})
+
+const signinSchema=zod.object({
+    username:zod.string().email(),
+    password:zod.string()
+})
+
+router.post("/signin",async (req,res)=>{
+    const body=req.body;
+    const{success}=signinSchema.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message:"Email already taken / Incorrect inputs"
+        })
+    }
+    
+    const existinguser=await User.findOne({
+        username:body.username,
+        password:body.password
+    });
+
+    if(existinguser){
+        const token=jwt.sign({
+            userId:existinguser._id
+        },JWT_SECRET);
+        
+        res.json({
+            token:token
+        })
+
+        return;
+    }
+
+    res.status(411).json({
+        message:"Error while loggin in"
+    })
+
+})
 
 module.exports=router;
