9e64c56300ad7a6c0580de4b20d75040fa8fdd93
nb-prog01 <nirajbhadoria007@gmail.com>
2024-03-30 23:15:42 +0530

    PAYTM UPDATE

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports={
+    JWT_SECRET:"One-Piece-is-real"
+}
+
+//OR
+
+// const JWT_SECRET="oOne-Piece-is-real"
+// module.exports=JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const cors=require('cors');
+
+const mainRouter=require('./routes/index');
+const app=express();
+const port=3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/v1',mainRouter)
 
 
+app.listen(port)
